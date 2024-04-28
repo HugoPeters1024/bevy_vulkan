a09314e2f8942966d388ddae2709adf60fcddd43
Hugo Peters <hpeters1024@gmail.com>
2024-04-28 21:27:13 +0200

    update deps

--- src/main.rs
+++ src/main.rs
@@ -153,13 +153,13 @@ fn setup(
     //    ),
     //));
 
-    //commands.spawn((
-    //    asset_server.load::<Gltf>("models/san_miquel.glb"),
-    //    TransformBundle::from_transform(
-    //        Transform::from_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2))
-    //            .with_scale(Vec3::splat(0.8)),
-    //    ),
-    //));
+    commands.spawn((
+        asset_server.load::<Gltf>("models/san_miquel.glb"),
+        TransformBundle::from_transform(
+            Transform::from_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2))
+                .with_scale(Vec3::splat(0.8)),
+        ),
+    ));
 
     //commands.spawn((
     //    asset_server.load::<Gltf>("models/bistro_interior.glb"),
@@ -169,13 +169,13 @@ fn setup(
     //    ),
     //));
 
-    commands.spawn((
-        asset_server.load::<Gltf>("models/rungholt.glb"),
-        TransformBundle::from_transform(
-            Transform::from_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2))
-                .with_scale(Vec3::splat(0.15)),
-        ),
-    ));
+    //commands.spawn((
+    //    asset_server.load::<Gltf>("models/rungholt.glb"),
+    //    TransformBundle::from_transform(
+    //        Transform::from_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2))
+    //            .with_scale(Vec3::splat(0.15)),
+    //    ),
+    //));
 
     //commands.spawn((
     //    asset_server.load::<Gltf>("models/living_room.glb"),
